f0de433e95d25fd70b6b40c23e4d7e32d50d257e
Darren Mulholland <dmulholl@tcd.ie>
2022-01-19 01:46:59 +0000

    Update documentation

--- docs/config.py
+++ docs/config.py
@@ -1,7 +1,3 @@
-# ------------------------------------------------------------------------------
-# Site Configuration File
-# ------------------------------------------------------------------------------
-
 title = "Argo"
-tagline = "A simple argument-parsing library for Go."
+tagline = "An argument-parsing library for Go."
 version = "Version 3.0.0"
